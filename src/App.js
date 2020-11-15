fd1dc4483ce833dcda2cd7fc0162dc22ee68081c
JPabloVera <pablvvera@gmail.com>
2020-11-15 19:24:58 -0300

    updating link

--- src/App.js
+++ src/App.js
@@ -1,28 +1,17 @@
 import React from 'react';
 import './App.css';
 import Nav from './Components/Nav/Nav'
-import world from './assets/world.png'
 import Search from './Components/Search/Search'
 import WorldView from './Components/WorldView/worldView'
 import {
   BrowserRouter,
   Switch,
   Route,
-  withRouter,
-  Link
+  withRouter
 } from "react-router-dom";
-import {useSelector} from 'react-redux';
-import {useGlobalDispatcher} from './redux/hooks'
+
 function App() {
-  const appStyle={
-    width:"100%",
-    height:"100%",
-    position:"absolute",
-    backgroundImage:"url("+world+")"
-  }
-  const estilo = {
-    backgroundColor:"#a2acbd"
-  }
+
 
   
   return (
@@ -32,7 +21,7 @@ function App() {
           <Nav/>
         </div>
         <Switch>
-            <Route exact path="/" render={() => (
+            <Route exact path="/ReactCovidTracker/" render={() => (
               <Search containerStyle={"searchContainer"} visibleContainer={true} searchInputContainerStyle={"searchInputContainerStyle"} searchInputStyle={"searchInputStyle"} 
               inputPlaceholder={"Countries,Provinces,Cities.."} validationClass={"validationError"}/>
             )}/>
